51046a9332cbece96b6aceb12c010916ff1d0ed2
Kira Corbin <madkatkeys@gmail.com>
2019-08-12 20:46:03 -0700

    added src files and deleted unwanted files

--- clicky-game/src/App.js
+++ clicky-game/src/App.js
@@ -1,29 +1,8 @@
-import React from 'react';
-import './App.css';
-import ImageCard from "./components/ImageCard"
-import Container from "./components/Container"
-import Row from "./components/Row"
-import Navbar from "./components/Navbar"
+import React from "react";
+import Game from "./components/Game";
 
 function App() {
-  return (
-    <div>
-
-
-      <Navbar />
-      <Container />
-      <Row>
-        <ImageCard imgUrl="http://picsum.photos/200/200" />
-        <ImageCard imgUrl="http://picsum.photos/200/200" />
-        <ImageCard imgUrl="http://picsum.photos/200/200" />
-      </Row>
-
-
-
-      {/* <ImageCard cardTitle="kIRA WAS HERE"/> */}
-
-    </div>
-  );
+  return <Game />;
 }
 
 export default App;
